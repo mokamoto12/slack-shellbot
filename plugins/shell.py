e6a7a43c14c23c42e9a3d2bcc671d1117f432191
mokamoto12 <mokamoto1208@gmail.com>
2017-08-30 01:17:53 +0900

    Add logging

--- plugins/shell.py
+++ plugins/shell.py
@@ -1,8 +1,17 @@
 from slackbot.bot import default_reply
 import subprocess
+from logging import getLogger, basicConfig, INFO
+
 
 @default_reply
 def shell(message):
-    text = message.body['text']
+    text = modify(message.body['text'])
     res = subprocess.run(text, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    basicConfig(format='%(asctime)s %(message)s', filename='exec.log', level=INFO)
+    logger = getLogger(__name__)
+    logger.info(text)
     message.send(res.stdout.decode())
+
+
+def modify(text):
+    return text.replace("“", '"').replace("”", '"').replace("‘", "'").replace("’", "'")
