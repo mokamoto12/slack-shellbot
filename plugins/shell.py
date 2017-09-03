80cd916bf77580d909517ad2de5fa24e484777da
mokamoto12 <mokamoto1208@gmail.com>
2017-09-04 01:22:32 +0900

    Update logging

--- plugins/shell.py
+++ plugins/shell.py
@@ -1,15 +1,12 @@
 from slackbot.bot import default_reply
 import subprocess
-from logging import getLogger, basicConfig, INFO
 
 
 @default_reply
 def shell(message):
     text = modify(message.body['text'])
+    print(text)
     res = subprocess.run(text, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, executable="/bin/bash")
-    basicConfig(format='%(asctime)s %(message)s', filename='exec.log', level=INFO)
-    logger = getLogger(__name__)
-    logger.info(text)
     message.send(res.stdout.decode())
 
 
